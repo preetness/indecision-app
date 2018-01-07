760eca03670f4db8038a08ddbc5dfaac44ed1892
Preet Dha <manpreetdha+github@gmail.com>
2018-01-06 21:33:15 -0800

    Initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+console.log('App.js is running!');
+
+// var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement(
+    "p",
+    null,
+    "This is JSX from app.js!"
+  );
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot); 
