895d6e7ba4f6e1526e872ffe87bb7f19dff7afd5
Preet Dha <manpreetdha+github@gmail.com>
2018-01-28 23:55:11 -0800

    Practice binding this and adding constructor to components

--- src/app.js
+++ src/app.js
@@ -40,8 +40,13 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
   handleRemoveAll() {
-    alert('Removing options...');
+    console.log(this.props.options);
+    //alert('Removing options...');
   }
   render() {
     return (
