a2dd3f542cf676573fd4a53d31db87e4d8053600
Preet Dha <manpreetdha+github@gmail.com>
2018-01-28 18:28:27 -0800

    Create subclass of class Person; practice overriding parent methods

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,8 +13,51 @@ class Person {
     }
 }
 
-const me = new Person('Preet Dha', 40);
+class Student extends Person {
+    constructor(name, age, major, university) {
+        super(name, age)
+        this.major = major
+        this.university = university
+    }
 
-const other = new Person();
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major} at the ${this.university}.`;
+        }
+        return description; 
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age)
+        this.homeLocation = homeLocation
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.hasHomeLocation()) {
+            return greeting += ` I'm visiting from ${this.homeLocation}.`
+        }
+    }
+}  
+
+const me = new Student('Preet Dha', 40, 'Computer Science', 'University of Washington');
+
+const other = new Student();
 console.log(me.getGreeting());
 console.log(me.getDescription());
+
+const mikhail = new Traveler('Mikhail Gorbachev', 78, 'Moscow')
+console.log(mikhail.getGreeting());
